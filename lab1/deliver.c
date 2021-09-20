c115b464a7147fa622dac35b693c7b240f21e513
Tonyzhang2000 <zhangshutong2000@gmail.com>
2021-09-19 20:59:13 -0400

    我又从guide上抄了一点，说实话这玩意有点烦！！！

--- lab1/deliver.c
+++ lab1/deliver.c
@@ -1,7 +1,53 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
 
-int main() {
+
+int main(int argc, char const * argv[]) {
+
+    if(argc != 3) {
+        printf("Invalid Input, exiting...\n");
+        return 0;
+    }
+
+    const char * portNum = argv[2];
+    const char * destIp = argv[1];
+
+    //Copied from Beej's guide
+    struct addrinfo hints;
+    struct addrinfo *serverinfo;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    int status = getaddrinfo(destIp, portNum, &hints, &serverinfo);
+    
+    if(status != 0) {
+        printf("Get address information error, exiting...\n");
+        return 0;
+    } else {
+        printf("Get address information successfully!");
+    }
+
+    int socketFD = socket(serverinfo->ai_family, serverinfo->ai_socktype, serverinfo->ai_protocol);
+
+    if(socketFD == -1) {
+        printf("Error! Could not create socket...\n");
+        return 0;
+    } else {
+        printf("Socket created successfully!\n");
+    }
+
+    int portNumber = atoi(argv[2]);
+
+
+    freeaddrinfo(serverinfo);
     printf("Hello world\n");
     return 0;
 }
