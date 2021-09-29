08a148be279ab0143a2051b8fab6ef40527a67d0
Tonyzhang2000 <zhangshutong2000@gmail.com>
2021-09-28 21:50:55 -0400

    copy things from lab1 to lab2 folder

---
+++ lab2/deliver.c
@@ -0,0 +1,98 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+
+#define MAXBUFLEN 100
+
+int main(int argc, char const * argv[]) {
+
+    if(argc != 3) {
+        printf("Invalid Input, exiting...\n");
+        return 0;
+    }
+
+    const char * portNum = argv[2];
+    const char * destIp = argv[1];
+
+    //Copied from Beej's guide
+    struct addrinfo hints;
+    struct addrinfo *serverinfo;    //will point to result
+
+    
+    memset(&hints, 0, sizeof hints);   //make struct empty
+    hints.ai_family = AF_INET;         //IPv4
+    hints.ai_socktype = SOCK_DGRAM;    //UDP
+    hints.ai_flags = AI_PASSIVE;       //Fill in my ip address
+
+    int status = getaddrinfo(destIp, portNum, &hints, &serverinfo);
+    
+    if(status != 0) {
+        printf("Get address information error, exiting...\n");
+        return 0;
+    } else {
+        printf("Get address information successfully!\n");
+    }
+
+    //get file descriptor
+    int socketFD = socket(serverinfo->ai_family, serverinfo->ai_socktype, serverinfo->ai_protocol);
+
+    if(socketFD == -1) {
+        printf("Error! Could not create socket...\n");
+        return 0;
+    } else {
+        printf("Socket created successfully!\n");
+    }
+
+    int portNumber = atoi(argv[2]);
+
+    //get the input from user ftp<file>
+    //check if file exist
+    //1. exist, send "ftp" to server
+    //2. exit 
+    char message[256], file_to_cp[256];
+    printf("Input a message of the following form\n\t ftp <file name>\n");
+    scanf("%s%s", message, file_to_cp);
+    if(strcmp(message, "ftp") == 0 ){
+        if(access(file_to_cp, F_OK) != -1){
+            int byteSend = sendto(socketFD, "ftp", 3, 0, serverinfo->ai_addr, serverinfo->ai_addrlen);
+            if( byteSend == -1){
+                printf("Error! Could not send message to server\n");
+                return 0;
+            }
+        }else{
+            printf("File doesn't exist!\n");
+            return 0;
+        }
+    }else{
+        printf("Error! Didn't find ftp entered\n");
+        return 0;
+    }
+
+
+    //get msg from server
+    //1. yes: print "A file transfer can start"
+    //2. no: exit 
+
+    char buff[MAXBUFLEN] = {0};
+    struct sockaddr_storage socketOutput;
+    socklen_t lenOutput = sizeof(socketOutput);
+
+    int msg_receive_from_server = recvfrom(socketFD, (void *) buff, MAXBUFLEN, 0,  (struct sockaddr *) &socketOutput, &lenOutput);
+
+    if(strcmp(buff, "yes") == 0){
+        printf("A file transfer can start\n");
+    }else{
+        printf("error, can't start file transfer\n");
+        return 0;
+    }
+
+    freeaddrinfo(serverinfo);
+    close(socketFD);         //prevent any read or write
+    return 0;
+}
+
---
+++ lab2/server.c
@@ -0,0 +1,80 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+#define MAXBUFLEN 100
+
+int main(int argc, char const * argv[]) {
+
+    if(argc != 2) {
+        printf("Invalid Input, exiting...\n");
+        return 0;
+    }
+
+    int portNumber = atoi(argv[1]);
+    int socketFD = socket(AF_INET, SOCK_DGRAM, 0);
+
+    if(socketFD  == -1) {
+        printf("Error! Could not create socket...\n");
+        return 0;
+    } else {
+        printf("Socket created successfully!\n");
+    }
+
+    printf("Server recieving on port %d...\n", portNumber);
+
+    struct sockaddr_in socketInput;
+    socketInput.sin_family = AF_INET;
+    socketInput.sin_addr.s_addr = htonl(INADDR_ANY);    //point to all local address
+    socketInput.sin_port = htons(portNumber);           //to the port thet I want to point to
+
+    int bindnum = bind(socketFD, (struct sockaddr *) &socketInput, (socklen_t) sizeof(socketInput));
+
+    if(bindnum == -1) {
+        printf("Bind fails, exiting...\n");
+        return 0;
+    } else {
+        printf("Bind successfully!\n");
+    }
+
+    //const int bufferSize = 44;
+    char buff[MAXBUFLEN] = {0};
+    struct sockaddr_storage socketOutput;
+    socklen_t lenOutput = sizeof(socketOutput);
+
+    int bytesReceive = recvfrom(socketFD, (void *) buff, MAXBUFLEN, 0, (struct sockaddr *) &socketOutput, &lenOutput);
+
+    //check if recvfrom is successful
+    if(bytesReceive == -1){
+        printf("recvfrom error...\n");
+    }else{
+        printf("%d bytes received!\n", bytesReceive);  //wo jue de ying gai xian xie deliver.c
+    }
+
+    //check if the client is sending "ftp" or not
+    if(strcmp(buff, "ftp") == 0){
+        if((sendto(socketFD, "yes", 3, 0, (struct sockaddr *) &socketOutput, lenOutput)) == -1){
+            printf("Error! Could not send message to client...\n");
+            return 0;
+        }else{
+            printf("Yes, Please continue ftp process\n");
+        }
+    }else{
+        //sendto client "no"
+        if((sendto(socketFD, "no", 2, 0, (struct sockaddr *) &socketOutput, lenOutput)) == -1){
+            printf("Error! Could not send message to client...\n");
+            return 0;
+        }else{
+            printf("No, the msg is not ftp\n");
+        }
+    }
+
+   close(socketFD);
+
+    return 0;
+}
+
